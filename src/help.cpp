f9ee4d2bd507d201857b39fc2eff195982cfcbab
Radomir Krawczykiewicz <kotwic4@gmail.com>
2017-06-07 10:47:33 +0200

    implement help

--- src/help.cpp
+++ src/help.cpp
@@ -1,6 +1,26 @@
 #include "help.h"
 
+const int command_number = 13;
+const char* commands[] = {
+        "add",
+        "checkout",
+        "clone",
+        "commit",
+        "config",
+        "diff",
+        "help",
+        "init",
+        "log",
+        "pull",
+        "push",
+        "reset",
+        "status"};
+
 int help(int argc, char *argv[]){
-    printf("Not defined help yet\n");
+    printf("Usage: tig <command> [<args>]\n");
+    printf("List of commands:\n");
+    for(int i = 0; i < command_number; i++){
+        printf("%s\n",commands[i]);
+    }
     return 0;
 }
